29fabe8d3471f6322401ca5151f603601ae8c62e
JoRPM <44869196+JoRPM@users.noreply.github.com>
2020-01-16 11:34:26 -0300

    teste

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ include("header.php");
 
 <label>Situação do login:<?php if(isset($_SESSION['login'])){echo $_SESSION['login'];}else{echo "Offline";} ?></label>
 
+<h1>Hello world</h1>
 <form method="post" action="login.php">
 <div>
 <label> Login do vendedor </label> 
