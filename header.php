32f042f6d18d117270f45864b6a5b46da3ee72c7
JoRPM <44869196+JoRPM@users.noreply.github.com>
2020-01-16 11:25:06 -0300

    header

--- header.php
+++ header.php
@@ -5,14 +5,26 @@ include("login.php");
 
 
 ?>
-
-
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>Bem Vindo, ...</title>
+  </head>
+  <body>
+  
+  <div class="menu">
+    <img src="img/certa.png" alt="">
+    <ul class="nav justify-content-center">
+    <button type="button" class="btn btn-outline" href="index.php">Home</button>
+    <button type="button" class="btn btn-outline" href="clientes.php">Controle de Clientes</button>
+    <button type="button" class="btn btn-outline" href="produtos.php">Controle de Produtos</button>
+    <button type="button" class="btn btn-outline" href="vendas.php">Vendas</button>
+    <button type="button" class="btn btn-outline" href="analise.php">Análise</button>  
+    </ul>    
+</div>
