7060824f3ee973c7a8074e17941fbafa239fc6af
Victor <victordcporto@gmail.com>
2020-01-24 11:55:22 -0300

    jquery, acessar sem header

--- acessar.php
+++ acessar.php
@@ -6,30 +6,30 @@ include("header.php");
 ?>
 
 <div class="container">
-<h1 class="h1" style="color:#FFFFFF">Viagem Certa</h1>
-<div id="login" class="card container">
-  <div class="card-body">
-    <h5 class="card-title" style="color: #38B6FF; text-align:center;">Login</h5>
-    <div class="container">
-<form action="login.php" method="post">
-  <div class="form-group">
-    <label for="exampleInputEmail1">Login</label>
-    <input placeholder="Digite o usuário..." type="text" class="form-control" name="vendedor" id="exampleInputEmail1" aria-describedby="emailHelp">
+  <h1 onclick="sumir()" class="h1" style="color:#FFFFFF">Viagem Certa</h1>
+  <div id="login" class="card container">
+    <div class="card-body">
+      <h5 class="card-title" style="color: #38B6FF; text-align:center;">Login</h5>
+      <div class="container">
+        <form action="login.php" method="post">
+          <div class="form-group">
+            <label for="exampleInputEmail1">Login</label>
+            <input placeholder="Digite o usuário..." type="text" class="form-control" name="vendedor" id="exampleInputEmail1" aria-describedby="emailHelp">
+          </div>
+          <div class="form-group">
+            <label for="exampleInputPassword1">Senha</label>
+            <input placeholder="Digite a senha..." type="password" name="senha" class="form-control" id="exampleInputPassword1">
+          </div>
+          <div class="text-center">
+            <button type="submit" name="logar" id="test" class="btn btn-outline login">Login</button>
+          </div>
+          <div class="text-center">
+            <a href="" name="" class="recuperar">Esqueceu a senha?</a>
+          </div>
+        </form>
+      </div>
+    </div>
   </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Senha</label>
-    <input placeholder="Digite a senha..." type="password" name="senha" class="form-control" id="exampleInputPassword1">
-  </div>
-  <div>
-  <button type="submit" name="logar" class="login">Login</button>
-</div>
-<div>
-  <a href="" name="" class="recuperar">Esqueceu a senha?</a>
-</div>
-</form>
-</div>
-  </div>
-</div>
 </div>
 
 
@@ -39,3 +39,6 @@ include("header.php");
 include("footer.php");
 
 ?>
+<script>
+  $("nav").css("display", "none");
+</script>
---
+++ js/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    function sumir(){
+       // $("body").css("vibilitiy", "hidden");
+        alert("test");
+    }
+  });
