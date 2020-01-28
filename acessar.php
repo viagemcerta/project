2464cc2996e63c19a93d1a326a56292a896f1f3b
matheus <matheusbastosddd@gmail.com>
2020-01-28 00:22:55 -0300

    Modifiquei algumas coisas da interface

--- acessar.php
+++ acessar.php
@@ -6,8 +6,9 @@ include("header.php");
 ?>
 
 <div class="container">
-<<<<<<< HEAD
-<h1 class="h1" style="color:#FFFFFF"> Viagem Certa</h1>
+<div>
+<h1 class="h1" style="color:#38B6FF; text-align: center;">Viagem Certa</h1>
+</div>
 <div id="login" class="card container">
   <div class="card-body">
     <h5 class="card-title" style="color: #38B6FF; text-align:center;">Login</h5>
@@ -31,32 +32,6 @@ include("header.php");
 </div>
   </div>
 </div>
-=======
-  <h1 onclick="sumir()" class="h1" style="color:#FFFFFF">Viagem Certa</h1>
-  <div id="login" class="card container">
-    <div class="card-body">
-      <h5 class="card-title" style="color: #38B6FF; text-align:center;">Login</h5>
-      <div class="container">
-        <form action="login.php" method="post">
-          <div class="form-group">
-            <label for="exampleInputEmail1">Login</label>
-            <input placeholder="Digite o usuário..." type="text" class="form-control" name="vendedor" id="exampleInputEmail1" aria-describedby="emailHelp">
-          </div>
-          <div class="form-group">
-            <label for="exampleInputPassword1">Senha</label>
-            <input placeholder="Digite a senha..." type="password" name="senha" class="form-control" id="exampleInputPassword1">
-          </div>
-          <div class="text-center">
-            <button type="submit" name="logar" id="test" class="btn btn-outline login">Login</button>
-          </div>
-          <div class="text-center">
-            <a href="" name="" class="recuperar">Esqueceu a senha?</a>
-          </div>
-        </form>
-      </div>
-    </div>
-  </div>
->>>>>>> origin/victor
 </div>
 
 
--- index.php
+++ index.php
@@ -6,9 +6,6 @@
     color: #FF914D;
   }
   
-  
-  
-  
   </style>
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
