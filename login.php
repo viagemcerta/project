b6baa4f68127924db370f1742a75f61f0b806c63
Victor <victordcporto@gmail.com>
2020-01-28 11:07:07 -0300

    atualizando login

--- login.php
+++ login.php
@@ -16,14 +16,19 @@ $queryLogin = "select * from login where vendedor = '$login' and senha = '$pass'
 $queryBanco = mysqli_query($conn, $queryLogin);
 $queryArray = mysqli_fetch_array($queryBanco);
 
+
 if($login == $queryArray['vendedor'] && $pass == $queryArray['senha']){ 
-  echo "logado com sucesso!";
-  $_SESSION['login'] = $login;
-  header("refresh:3;url= painel.php");
-}else{ 
-  echo "Erro ao tentar fazer loginm";
-  header("refresh:3;url= index.php");
+  echo "test ok";
 }
+
+// if($login == $queryArray['vendedor'] && $pass == $queryArray['senha']){ 
+//   echo "logado com sucesso!";
+//   $_SESSION['login'] = $login;
+//   header("refresh:3;url= painel.php");
+// }else{ 
+//   echo "Erro ao tentar fazer loginm";
+//   header("refresh:3;url= index.php");
+// }
 
 
 
