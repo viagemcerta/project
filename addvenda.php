77a48f9235bcfef2d6f0c2dd2e688ef98bf6de93
Victor <victordcporto@gmail.com>
2020-02-03 20:32:36 -0300

    vendas

--- addvenda.php
+++ addvenda.php
@@ -4,21 +4,34 @@
 if(isset($_POST['addvenda'])){ 
 
 $cpf = $_POST['cpf'];
-$produto = $_POST['produto']; 
-$categoria= $_POST['categoria']; 
-$preço = $_POST['preço'];
-$destino = $_POST['destino']; 
-$partida = $_POST['partida']; 
-$dta_partida = $_POST['dta_partida']; 
-$dta_chegada = $_POST['dta_chegada'];
+$idProduto = $_POST['id'];
+$valor = $_POST['preco'];
+$atualdata = date('yy/m/d');
+include("connection.php");
+
+//buscando cliente
+$queryCliente = "select * from addcliente where CPF = '$cpf'";
+$clienteBanco = mysqli_query($conn, $queryCliente);
+$clienteArray = mysqli_fetch_array($clienteBanco);
+$idCliente = $clienteArray['id_cliente'];
+
+$queryProduto = "select * from produtos where id_produto = $idProduto";
+$produtoBanco = mysqli_query($conn, $queryProduto);
+$produtoArray = mysqli_fetch_array($produtoBanco);
+$estoque = $produtoArray['qnt_estoque'];
+$estoque = $estoque - 1;
+
 
 include('connection.php');
 
-$query = "insert into vendas(cpf,produto,categoria,preço,destino,partida,dta_partida,dta_chegada)
-values('$cpf','$produto','$categoria','$preço','$destino','$partida','$dta_partida','$dta_chegada')";
+$query = "insert into vendas(id_cliente,id_produto,valor,dta_venda)
+values('$idCliente', '$idProduto','$valor','$atualdata')";
 
 if(mysqli_query($conn, $query)){ 
+    $queryUpdate = "UPDATE produtos set qnt_estoque = $estoque where id_produto = $idProduto";
+    $produtoBancoUpdate = mysqli_query($conn, $queryUpdate);
     echo "venda cadastrada"; 
+
 }else{ 
     echo "err";
 }
