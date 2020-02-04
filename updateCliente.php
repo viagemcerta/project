05f4921daa8012590cb23733c0380efad01f2f08
JoRPM <44869196+JoRPM@users.noreply.github.com>
2020-02-04 08:45:32 -0300

    changes

--- updateCliente.php
+++ updateCliente.php
@@ -11,14 +11,9 @@ $nome = $_POST['nome'];
 $email = $_POST['email']; 
 $cpf = $_POST['CPF']; 
 $dta_nascimento = $_POST['dta_nascimento'];
-$destino = $_POST['destino']; 
-$partida = $_POST['partida']; 
-$dta_partida = $_POST['dta_partida']; 
-$dta_chegada = $_POST['dta_chegada'];
 
 
-   $query ="update addcliente set nome ='$nome', email = '$email', CPF='$cpf', dta_nascimento = '$dta_nascimento',
-   destino = '$destino', partida = '$partida', dta_partida = '$dta_partida', dta_chegada = '$dta_chegada' where
+   $query ="update addcliente set nome ='$nome', email = '$email', CPF='$cpf', dta_nascimento = '$dta_nascimento' where
    id_cliente = $id";
     if(mysqli_query($conn, $query)){
        echo ' <script>window.location.href = "listClientes.php"</script>';
