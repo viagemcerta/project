082ef05e6fe8ca69db64c12afa50942c4311c099
Victor <victordcporto@gmail.com>
2020-02-06 09:03:27 -0300

    Final Version 1.0

--- addcliente.php
+++ addcliente.php
@@ -8,11 +8,14 @@ $nome = $_POST['nome'];
 $email = $_POST['email']; 
 $cpf = $_POST['CPF']; 
 $dta_nascimento = $_POST['dta_nascimento'];
+$atualdata = date('yy/m/d');
 include('connection.php');
 
 
-$query = "insert into addcliente(nome,email,CPF,dta_nascimento,situacao)
-values('$nome','$email','$cpf','$dta_nascimento','ativo')";
+
+
+$query = "insert into addcliente(nome,email,CPF,dta_nascimento,situacao,dataCadastro)
+values('$nome','$email','$cpf','$dta_nascimento','ativo','$atualdata')";
 
 if(mysqli_query($conn, $query)){ 
     echo ' <script>window.location.href = "formClientes.php"</script>';
