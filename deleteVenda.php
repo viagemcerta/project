858b570ca261bda70589af836e22ee52df7eff72
Victor <victordcporto@gmail.com>
2020-02-06 22:54:18 -0300

    reabastecimento

--- deleteVenda.php
+++ deleteVenda.php
@@ -4,11 +4,23 @@ $id = $_GET['id'];
 
 include("connection.php"); 
 
+$queryProd = "select * from vendas v, produtos p where  v.id_produto = p.id_produto";
+$queryBanco = mysqli_query($conn, $queryProd);
+$queryArray = mysqli_fetch_array($queryBanco);
+$idProd = $queryArray['id_produto'];
+$estoque = $queryArray['qnt_estoque'];
+
+$estoque = $estoque + 1;
+
+
+$queryProd2 = "UPDATE produtos set qnt_estoque = $estoque where id_produto = $idProd ";
+
+
 $query = "delete FROM vendas where id_venda = $id";
 
 
 if(mysqli_query($conn, $query)){ 
-    echo "Delete success! ";
+    mysqli_query($conn, $queryProd2);
     echo ' <script>window.location.href = "listVendas.php"</script>';
 }else{ 
     echo "err";
